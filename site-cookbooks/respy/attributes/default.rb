0a57cf4dbf8b674b2b892d8566ea640ef2d80418
peisenha <eisenhauer@policy-lab.org>
2016-07-27 20:32:08 +0200

    edits

--- site-cookbooks/respy/attributes/default.rb
+++ site-cookbooks/respy/attributes/default.rb
@@ -1,2 +1,2 @@
-default['respy_development']['home'] = '/home/vagrant'
-default['respy_development']['user'] = 'vagrant' 
+default['respy_development']['home'] = '/home/ubuntu'
+default['respy_development']['user'] = 'ubuntu' 
